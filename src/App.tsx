190f872652fc12b09b7d28578ba1ff293c905961
DaniMendoza <elchino.dmen@gmail.com>
2026-01-29 11:21:38 -0300

    Set basename for BrowserRouter to '/my-digital-space'

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/my-digital-space">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
