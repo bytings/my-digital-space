b9d24bea9b8cd2bdd0a6780b6629154252825071
DaniMendoza <elchino.dmen@gmail.com>
2026-01-29 11:41:18 -0300

    Remove basename from BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/my-digital-space">
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
